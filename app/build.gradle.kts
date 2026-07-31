894102dc32680ae0ec8e8f6c7495c5fc9b5115d3
power690 <2103890018@qq.com>
2026-07-31 01:50:39 +0000

    42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xiaowei.player"
         minSdk = 23
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
